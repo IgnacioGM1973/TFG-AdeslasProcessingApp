292e967fabeca54b7305db50840e035af50e3f42
IgnacioGM1973 <I.GUIJARRO@HOTMAIL.ES>
2026-04-29 21:11:47 +0200

    Proyecto TFG v02

--- Proyecto_intermodular_Desarrollo_de_Aplicaciones/AdeslasProcessingAppProyecto/AdeslasProcessingApp/Modulo1_ConexionBD.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports System.IO
-
-Module Modulo1_ConexionBD
-
-    'Public accessConnString As String = "Provider=Microsoft.ACE.OLEDB.16.0;Data Source=" & RutaPrincipal & "\bd\ADESLAS.accdb"
-    Public accessConnString As String = "Provider=Microsoft.ACE.OLEDB.16.0;Data Source=" & Path.Combine(RutaBD, "ADESLAS.accdb")
-
-End Module
